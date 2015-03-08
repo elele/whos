dcbacd668916d03744fcab234f0bbd9f3855a183
段磊 <elele_2005@qq.com>
2015-03-08 17:08:07 +0800

    好友列表

--- app/models/whos_friend.rb
+++ app/models/whos_friend.rb
@@ -3,4 +3,5 @@ class WhosFriend < ActiveRecord::Base
   belongs_to :friend, :class_name => "User"
 
   scope :white_list, -> { where(:black => false) }
+  default_scope { order(:recent_time => :desc) }
 end
