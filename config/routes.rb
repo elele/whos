d6a7f9f6f637fa5825cdbcec04b16be30de10006
段磊 <elele_2005@qq.com>
2015-03-08 20:38:44 +0800

    好友列表

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       post :send_message
       post :messages
       post :blacklist
+      post :all_friend
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
