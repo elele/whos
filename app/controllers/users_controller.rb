3cc4b54687c354e81cdda3c6aa0ba9ff71b69800
段磊 <elele_2005@qq.com>
2015-03-08 17:29:52 +0800

    好友列表

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -98,7 +98,7 @@ class UsersController < ApplicationController
   def friends_josn(friends)
     jsons = []
     friends.each do |friend|
-      jsons << {user_name: friend.user.user_name, remark: friend.remark || '',
+      jsons << {user_name: friend.friend.user_name, remark: friend.remark || '',
                 recent_time: friend.recent_time.to_i, user_id: friend.friend_id}
     end
     jsons
