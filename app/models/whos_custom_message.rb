1aefb8de365ed1260693bed9425711a7718ee5d6
段磊 <elele_2005@qq.com>
2015-03-10 21:46:22 +0800

    fix

--- app/models/whos_custom_message.rb
+++ app/models/whos_custom_message.rb
@@ -1,9 +1,8 @@
 class WhosCustomMessage < ActiveRecord::Base
   include OPE::Jpush
   belongs_to :user
-  has_one :whos_user_device ,:through => :user
   belongs_to :reveice, class_name: "User"
-
+  has_one :whos_user_device ,:through => :reveice
   after_create :pushout
 
   def options
