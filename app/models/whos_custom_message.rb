19218692bcd668497ccfbaa9793767e1054b055d
段磊 <elele_2005@qq.com>
2015-03-19 21:16:00 +0800

    fix

--- app/models/whos_custom_message.rb
+++ app/models/whos_custom_message.rb
@@ -17,8 +17,10 @@ class WhosCustomMessage < ActiveRecord::Base
 
   def set_recent_time
     who_friend = WhosFriend.where(user: user, friend: reveice).first
-    if who_friend
+    reveice_friend = WhosFriend.where(user: friend, friend: user)
+    if who_friend && reveice_friend
       who_friend.update_columns(recent_time: Time.now)
+      reveice_friend.update_columns(recent_time: Time.now)
     end
   end
 
